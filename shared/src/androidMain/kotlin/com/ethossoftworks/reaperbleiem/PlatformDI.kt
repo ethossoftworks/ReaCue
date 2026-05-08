c715dc2dd290dad0700a49b90caf29249968d413
Ryan Mitchener <ryan@outsidesource.com>
2026-05-07 22:20:52 -0400

    Fixes

--- shared/src/androidMain/kotlin/com/ethossoftworks/reaperbleiem/PlatformDI.kt
+++ shared/src/androidMain/kotlin/com/ethossoftworks/reaperbleiem/PlatformDI.kt
@@ -2,7 +2,7 @@ package com.ethossoftworks.reaperbleiem
 
 import android.content.Context
 import com.ethossoftworks.reaperbleiem.lib.ActivityHolder
-import com.ethossoftworks.reaperbleiem.service.bluetooth.KmpBle
+import com.ethossoftworks.reaperbleiem.service.bluetooth.KmpBleCentralManager
 import com.outsidesource.oskitkmp.storage.KmpKvStore
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -12,7 +12,7 @@ actual data class PlatformContext(val context: Context)
 actual fun platformModule(platformContext: PlatformContext): Module = module {
     single { KmpKvStore(appContext = platformContext.context) }
     single { ActivityHolder() }
-    single { KmpBle(platformContext.context) }
+    single { KmpBleCentralManager(platformContext.context) }
 }
 
 actual fun platformMockModule(platformContext: PlatformContext): Module = module {}
