d7b98f751bb70d5fc3e19a3c7d56e0307e65b87c
Ryan Mitchener <ryan@outsidesource.com>
2026-05-17 20:36:43 -0400

    Fixed lua script

--- reaperConfigs/BleIem.lua
+++ reaperConfigs/BleIem.lua
@@ -1,6 +1,13 @@
+local last_project = nil
+
 function ble_iem()
-  local null, project_fn = reaper.EnumProjects(-1)
-  reaper.SetExtState("BleIem", "ProjectName", project_fn, false)
+  local _, project_fn = reaper.EnumProjects(-1)
+
+  if last_project ~= project_fn then
+    last_project = project_fn
+    reaper.SetExtState("BleIem", "ProjectName", project_fn, false)
+  end
+
   reaper.defer(ble_iem)
 end
 
